d11330e00a0a60e77878eba9180fd7b8cacecf70
Sagar Prajapati <sagarprajapatibhai@gmail.com>
2025-10-04 09:57:19 +0530

    Removed folder from repository

--- Day_02/App.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import * as ReactDOM from "react-dom/client";
-
-const heading = React.createElement("h1", { id: "heading" }, "Hello World from React 🛫🛫🛫");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(heading);
